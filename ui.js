0753e0b4726a211d8ae7a7235e4ed51cc0db197e
jack0lantern <j_j_jiang@yahoo.com>
2016-03-11 23:21:42 -0600

    added ui.js, cut custom palette for now

---
+++ ui.js
@@ -0,0 +1,11 @@
+// User Interface module
+var ui = (function () {
+    var settings = {
+    mode: 'line',                   // 'line', 'block', 'bucket'
+    fillMode: 'transparent',        // 'fill', 'transparent', 'custom'
+    fillChar: CHAR_SPACE,
+        //    activeChar: null,     // potentially for custom chars
+    currentTab: 'draw',
+    pasteTransparent: false
+};
+})();
