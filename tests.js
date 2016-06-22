16785b3dc9cfb27e162592d3311cde116eb7bd50
jack0lantern <j_j_jiang@yahoo.com>
2016-06-21 22:32:07 -0500

    updated ellipse test

--- tests.js
+++ tests.js
@@ -18,7 +18,7 @@ QUnit.test( "Box getRow1", function( assert ) {
 });
 
 QUnit.test( "Box getEllipseRanges0", function( assert ) {
-    assert.ok(testBox.getEllipseRanges(0, 0) === [[0, 0]], "Passed!" );
+    assert.ok(testBox.getEllipseRanges(0, 0).equals([[0, 0]]), "Passed!" );
 });
 
 /* Things left to test:
@@ -30,3 +30,35 @@ QUnit.test( "Box getEllipseRanges0", function( assert ) {
  * paste
  *
  */
+
+// http://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
