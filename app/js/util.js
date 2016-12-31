486414c9758f31f3ee72ddbb1f90a51189901a60
jack0lantern <j_j_jiang@yahoo.com>
2016-12-31 16:19:25 -0600

    reverseList unnecessary, can use Array.reverse

--- app/js/util.js
+++ app/js/util.js
@@ -16,18 +16,6 @@ function mergeOverSpace(subject, tgt) {
         return null;
 }
 
-// Takes a list, returns a reversed version of that list without modifying it
-// (and this is because arguments are passed by value)
-function reverseList(list) {
-    var tempVal;
-    for (var i = 0; i < Math.floor(list.length/2); i++) {
-        tempVal = list[i];
-        list[i] = list[list.length - 1 - i];
-        list[list.length - 1 - i] = tempVal;
-    }
-    return list;
-}
-
 function inRange(value, a, b) {
     return (a <= value && value <= b) || (b <= value && value <= a);
 }
