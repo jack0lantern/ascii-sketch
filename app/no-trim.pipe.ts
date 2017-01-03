32d80f116684ece83454f2f8160b651eca57e1d6
jack0lantern <j_j_jiang@yahoo.com>
2017-01-03 01:28:45 -0600

    initial angular 2 rebuild commit. has toh contents

---
+++ app/no-trim.pipe.ts
@@ -0,0 +1,15 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+//  * Prevents the trim (default) of the input
+//  * Usage:
+//  *   value | noTrim
+//  * Example:
+//  *   {{ 2 |  exponentialStrength:10}}
+//  *   formats to: 1024
+
+// @Pipe({name: 'noTrim'})
+// export class NoTrimPipe implements PipeTransform {
+//   transform: function(value: string): string {
+//     return value;
+//   }
+// }
